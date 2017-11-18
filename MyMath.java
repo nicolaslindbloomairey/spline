eb794f7433a2918945f5002fb6ec48666b30a0c5
Nicolas <nicolas.lindbloom.airey@gmail.com>
2017-11-17 19:30:54 -0800

    works

--- MyMath.java
+++ MyMath.java
@@ -1,4 +1,6 @@
-public static class MyMath {
+import java.util.ArrayList;
+
+public class MyMath {
     public static int factorial(int f) {
         if (f == 0) {
             return 1;
@@ -10,23 +12,64 @@ public static class MyMath {
         return fact;
     }
 
+    //pascals triangle coefficients
     public static int binomialCoefficient(int n, int k) {
         return (factorial(n) / (factorial(k) * factorial(n-k)));
     }
 
+    //recursive defintion of bezier curve of degree n for n+1 points
+    public static Curve bezier(double[] x, double[] y, double dt) {
+        ArrayList<Double> xresult = new ArrayList<Double>();
+        ArrayList<Double> yresult = new ArrayList<Double>();
+        int n = x.length -1;
+
+        for (double t = 0.0; t<1.0; t+=dt) {
+
+            double xt = 0., yt = 0.;
+            for (int i = 0; i<=n; i+=1) {
+                xt += (binomialCoefficient(n, i) * Math.pow(1.0-t, n-i) * Math.pow(t, i) * x[i] );
+                yt += (binomialCoefficient(n, i) * Math.pow(1.0-t, n-i) * Math.pow(t, i) * y[i] );
+            }
+            xresult.add(xt);
+            yresult.add(yt);
+        }
+
+        int[] xarray = new int[xresult.size()];
+        int[] yarray = new int[yresult.size()];
+
+        for (int i = 0; i<xresult.size(); i++) {
+            xarray[i] = (int)Math.ceil(xresult.get(i));
+            yarray[i] = (int)Math.ceil(yresult.get(i));
+        }
+        int points = (int) (1.0/dt);
+        Curve curve = new Curve(xarray,yarray, points);
+        return curve;
+    }
+
+    //only works with four points
+    public static Curve cubicbezier(Point p0, Point p1, Point p2, Point p3, double dt) {
+        int n = (int) (1.0/dt);
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (double t = 0.0; t<=1.0; t+=dt) {
+            int i = (int)(t/dt);
+            x[i] = (int) ( (Math.pow((1.0 - t), 3) * p0.x) + (3 * Math.pow((1.0-t), 2) * t * p1.x) + (3 * Math.pow(t, 2) * (1.0-t) * p2.x) + (Math.pow(t, 3) * p3.x));
+            y[i] = (int) ( (Math.pow((1.0 - t), 3) * p0.y) + (3 * Math.pow((1.0-t), 2) * t * p1.y) + (3 * Math.pow(t, 2) * (1.0-t) * p2.y) + (Math.pow(t, 3) * p3.y));
+        }
+        Curve temp = new Curve(x, y, n);
+        return temp;
+    }
+
+    //catmullrom cubic spline
     public static Curve catmullrom(Point p0, Point p1, Point p2, Point p3, double dt) {
         int n = (int) (1.0/dt);
         int[] x = new int[n];
         int[] y = new int[n];
-        for (double t = 0.0; t<1.0; t+=dt) {
+        for (double t = 0.0; t<=1.0; t+=dt) {
             int i = (int)(t/dt);
             x[i] = (int) (0.5 * ( (2*p1.x) + ((0 - p0.x + p2.x)*t) + (((2*p0.x) - (5*p1.x) + (4*p2.x) - p3.x)*(Math.pow(t, 2))) + (((-1*p0.x) + (3*p1.x) - (3*p2.x) + p3.x)*(Math.pow(t, 3)))));
             y[i] = (int) (0.5 * ( (2*p1.y) + ((0 - p0.y + p2.y)*t) + (((2*p0.y) - (5*p1.y) + (4*p2.y) - p3.y)*(Math.pow(t, 2))) + (((-1*p0.y) + (3*p1.y) - (3*p2.y) + p3.y)*(Math.pow(t, 3)))));
         }
-        double t = 1.0;
-        int i = n-1;
-            x[i] = (int) (0.5 * ( (2*p1.x) + ((0 - p0.x + p2.x)*t) + (((2*p0.x) - (5*p1.x) + (4*p2.x) - p3.x)*(Math.pow(t, 2))) + (((-1*p0.x) + (3*p1.x) - (3*p2.x) + p3.x)*(Math.pow(t, 3)))));
-            y[i] = (int) (0.5 * ( (2*p1.y) + ((0 - p0.y + p2.y)*t) + (((2*p0.y) - (5*p1.y) + (4*p2.y) - p3.y)*(Math.pow(t, 2))) + (((-1*p0.y) + (3*p1.y) - (3*p2.y) + p3.y)*(Math.pow(t, 3)))));
         Curve temp = new Curve(x, y, n);
         return temp;
     }
--- Screen.java
+++ Screen.java
@@ -5,9 +5,11 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
 
-public class Screen extends JPanel implements MouseListener {
+public class Screen extends JPanel implements MouseListener, KeyListener {
 
     public static final int HEIGHT=600, WIDTH=800;
     ArrayList<Point> cp;
@@ -16,38 +18,9 @@ public class Screen extends JPanel implements MouseListener {
         this.setLayout(null);
         this.setFocusable(true);
         addMouseListener(this);
+        addKeyListener(this);
 
         cp = new ArrayList<Point>();
-    }
-
-
-    public static Curve bezier(int[] x, int[] y) {
-        ArrayList<Integer> xresult = new ArrayList<Integer>();
-        ArrayList<Integer> yresult = new ArrayList<Integer>();
-        int n = x.length -1;
-
-        for (int i = 0; i<=n; i++) {
-            xresult.add( (
-        }
-
-    }
-
-
-    public static Curve cubicbezier(Point p0, Point p1, Point p2, Point p3, double dt) {
-        int n = (int) (1.0/dt);
-        int[] x = new int[n];
-        int[] y = new int[n];
-        for (double t = 0.0; t<1.0; t+=dt) {
-            int i = (int)(t/dt);
-            x[i] = (int) ( (Math.pow((1.0 - t), 3) * p0.x) + (3 * Math.pow((1.0-t), 2) * t * p1.x) + (3 * Math.pow(t, 2) * (1.0-t) * p2.x) + (Math.pow(t, 3) * p3.x));
-            y[i] = (int) ( (Math.pow((1.0 - t), 3) * p0.y) + (3 * Math.pow((1.0-t), 2) * t * p1.y) + (3 * Math.pow(t, 2) * (1.0-t) * p2.y) + (Math.pow(t, 3) * p3.y));
-        }
-        double t = 1.0;
-        int i = n-1;
-        x[i] = (int) ( (Math.pow((1.0 - t), 3) * p0.x) + (3 * Math.pow((1.0-t), 2) * t * p1.x) + (3 * Math.pow(t, 2) * (1.0-t) * p2.x) + (Math.pow(t, 3) * p3.x));
-        y[i] = (int) ( (Math.pow((1.0 - t), 3) * p0.y) + (3 * Math.pow((1.0-t), 2) * t * p1.y) + (3 * Math.pow(t, 2) * (1.0-t) * p2.y) + (Math.pow(t, 3) * p3.y));
-        Curve temp = new Curve(x, y, n);
-        return temp;
     }
 
     public Dimension getPreferredSize() {
@@ -68,19 +41,37 @@ public class Screen extends JPanel implements MouseListener {
             g.drawOval((int)(cp.get(i).x)-2, (int)(cp.get(i).y)-2, 4, 4);
         }
 
+        /* Catmull Rom drawing
         if (cp.size()>=4) {
             for (int i = 1; i<cp.size()-2; i++) {
-                Curve curve = catmullrom(cp.get(i-1), cp.get(i), cp.get(i+1), cp.get(i+2), 0.1);
+                Curve curve = MyMath.catmullrom(cp.get(i-1), cp.get(i), cp.get(i+1), cp.get(i+2), 0.1);
                 g.drawPolyline(curve.x, curve.y, curve.n);
             }
         }
+        */
+
+        double[] x = new double[cp.size()];
+        double[] y = new double[cp.size()];
+        for (int i = 0; i<cp.size(); i++) {
+            x[i] = cp.get(i).x;
+            y[i] = cp.get(i).y;
+        }
+        Curve curve = MyMath.bezier(x, y, 0.01);
+        g.drawPolyline(curve.x, curve.y, curve.n);
         
+    }
+
+    public void keyPressed(KeyEvent e) {}
+    public void keyReleased(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyCode()==KeyEvent.VK_ENTER) {
+            System.out.println("enter");
+        }
     }
 
     public void mousePressed(MouseEvent e) {}
     public void mouseReleased(MouseEvent e) {}
     public void mouseClicked(MouseEvent e) {
-        if (e.get
         cp.add(new Point(e.getX(), e.getY()));
         System.out.println(e.getX() + " " + e.getY());
         repaint();
